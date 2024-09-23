f0f5d2094894f843e657dc717ea25304cf5d1090
Davoyaaj <davoyaaj@mail.uc.edu>
2024-09-22 20:42:03 -0700

    Revert "Input Actions"
    This reverts commit 045dc0219dddb972350fd46f4261d7d5708537ae.

--- UC NetCode 2024 Fall/Assets/App/Resource/Scripts/UI/UI_NetManager.cs
+++ UC NetCode 2024 Fall/Assets/App/Resource/Scripts/UI/UI_NetManager.cs
@@ -7,51 +7,35 @@ using UnityEngine.UI;
 public class UI_NetManager : NetworkBehaviour
 {
 
-    [SerializeField] private Button _serverBttn, _clientBttn, _hostBttn, _startBttn;
-
-    [SerializeField] private GameObject _connectionBttnGroup;
-
-    [SerializeField] private SpawnController _mySpawnController;
+    [SerializeField] private Button _serverBttn, _clientBttn, _hostBttn;
 
     // Start is called before the first frame update
     void Start()
     {
-        _startBttn.gameObject.SetActive(false);
-        if (_serverBttn != null) _serverBttn.onClick.AddListener(ServerClick);
-        if (_clientBttn != null) _clientBttn.onClick.AddListener(ClientClick);
-        if (_hostBttn != null) _hostBttn.onClick.AddListener(HostClick);
-        if (_startBttn != null) _startBttn.onClick.AddListener(StartClick);
+        _serverBttn.onClick.AddListener(ServerClick);
+        _clientBttn.onClick.AddListener(ClientClick);
+        _hostBttn.onClick.AddListener(HostClick);
 
     }
 
-    private void StartClick()
-    {
-        //hook up spawning here.
-        if (IsServer)
-        {
-            _mySpawnController.SpawnAllPlayers();
-            _startBttn.gameObject.SetActive(false);
-        }
-    }
+
 
     private void ServerClick()
     {
-        NetworkManager.Singleton.StartServer();      // Starts the NetworkManager as just a server (that is, no local client).
-        _connectionBttnGroup.SetActive(false);
-        _startBttn.gameObject.SetActive(true);
+        NetworkManager.Singleton.StartHost();      // Starts the NetworkManager as just a server (that is, no local client).
+        this.gameObject.SetActive(false);
 
     }
 
     private void ClientClick()
     {
         NetworkManager.Singleton.StartClient();        // Starts the NetworkManager as both a server and a client (that is, has local client)
-        _connectionBttnGroup.SetActive(false);
+        this.gameObject.SetActive(false);
     }
 
     private void HostClick()
     {
         NetworkManager.Singleton.StartHost();      // Starts the NetworkManager as just a client.
-        _connectionBttnGroup.SetActive(false);
-        _startBttn.gameObject.SetActive(true);
+        this.gameObject.SetActive(false);
     }
 }
